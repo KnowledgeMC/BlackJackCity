ac57b2248e060a6d1fe9a4c822657e076a0c1870
Roy Hwang <tupaclypse@gmail.com>
2016-10-10 12:16:48 -0700

    add LoDash, some CSS, basic states in JS

--- js/main.js
+++ js/main.js
@@ -2,4 +2,38 @@ console.log("JS Linked!")
 
 var dealerTotal = 0;
 var playerTotal = 0;
-var won = false;
+var gameWon = false;
+
+var bankroll = 1000; //Might implement if I have time
+
+var deck = {};
+
+var dealerCards = [];
+var playerCards = [];
+
+
+//deal function that picks random number between 1 and 52,
+//and returns the number
+var deal = function(){
+  var card = Math.floor(Math.random() * 52) + 1;
+  return card;
+};
+
+
+
+$('#start').on('click', function(){
+  alert('Starting game now!');
+});
+
+// functions:
+// draw/render
+
+// deal
+
+// gameWon?
+
+// calculate playerTotal
+
+// calculate dealerTotal
+
+
